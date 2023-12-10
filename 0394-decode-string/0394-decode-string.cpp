747e84581e9c8adf48aedfb944fd41fb65f773e3
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2023-12-10 17:27:40 +0530

    Time: 3 ms (39.72%), Space: 6.8 MB (73.81%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -10,13 +10,12 @@ public:
                     st.pop();
                 }
                 st.pop();
-                string x;
+                int val = 0,mult=1;
                 while(!st.empty() && st.top()>='0' && st.top()<='9'){
-                    x+=st.top();
+                    val = val + mult*(st.top()-'0');
+                    mult *= 10;
                     st.pop();
                 }
-                reverse(x.begin(),x.end());
-                int val = stoi(x);
                 for(int i=0;i<val;i++){
                     for(int j=temp.size()-1;j>=0;j--){
                         st.push(temp[j]);
