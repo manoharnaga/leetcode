05b65f097bf178dc5eb4ec67ba33a4f47ece7983
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2023-12-14 12:00:00 +0530

    Time: 350 ms (80.58%), Space: 90.1 MB (83.74%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& a) {
         int n=a.size();
-        sort(a.begin(),a.end(),[](const auto& x,const auto& y){
-            if(x[1]==y[1]) return x[0]<y[0];
-            return x[1]<y[1];
-        });
-        vector<int> x = a[0];
-        if(n==1) return 1;
-        int ans = 0;
+        sort(a.begin(),a.end());
+        int cnt = 0;
+        int end = a[0][1];
         for(int i=1;i<n;i++){
-            if(x[1]>=a[i][0]){
-                ans++;
+            if(end>=a[i][0]){
+                end = min(end,a[i][1]);
             }
             else{
-                x = a[i];
+                cnt++;
+                end = a[i][1];
             }
         }
-        // ans--; // don't consider first 
-        return (n-ans);// just return the number of actual non-overlapping intervals
-        // the other removed intervals will overlap with one of the non-overlapping intervals
+        return (cnt+1);
     }
 };
