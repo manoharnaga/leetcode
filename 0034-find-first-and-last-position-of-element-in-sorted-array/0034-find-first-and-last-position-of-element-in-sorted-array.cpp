3109d679a6ddf2ff797204754f6250ad7b7a0f5c
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2023-12-20 18:47:37 +0530

    Time: 3 ms (92.57%), Space: 14.2 MB (16.53%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -3,33 +3,34 @@ public:
     
     vector<int> searchRange(vector<int>& a, int x) {
         int n = a.size();
-        int low = 0, high = n-1;
+        int low1 = 0, high1 = n-1,low2 = 0,high2 = n-1;
         int x1=-1,x2=-1;
-        while(low<=high){
-            int mid = (high-low)/2 + low;
-            if(a[mid]==x){
-                x1 = mid;
-                high = mid-1;
+        while(low1<=high1 || low2<=high2){
+            int mid1 = (high1-low1)/2 + low1;
+            int mid2 = (high2-low2)/2 + low2;
+            if(low1<=high1){
+                if(a[mid1]==x){
+                    x1 = mid1;
+                    high1 = mid1-1;
+                }
+                else if(a[mid1]>x){
+                    high1 = mid1-1;
+                }
+                else{
+                    low1 = mid1+1;
+                }
             }
-            else if(a[mid]>x){
-                high = mid-1;
-            }
-            else{
-                low = mid+1;
-            }
-        }
-        low = 0, high = n-1;
-        while(low<=high){
-            int mid = (high-low)/2 + low;
-            if(a[mid]==x){
-                x2 = mid;
-                low = mid+1;
-            }
-            else if(a[mid]>x){
-                high = mid-1;
-            }
-            else{
-                low = mid+1;
+            if(low2<=high2){
+                if(a[mid2]==x){
+                    x2 = mid2;
+                    low2 = mid2+1;
+                }
+                else if(a[mid2]>x){
+                    high2 = mid2-1;
+                }
+                else{
+                    low2 = mid2+1;
+                }
             }
         }
         return {x1,x2};
