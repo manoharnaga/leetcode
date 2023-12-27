da3ff5e9665abc618889ed26396edd7f0e1b7789
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2023-12-27 09:22:53 +0530

    Time: 5 ms (29.48%), Space: 9.6 MB (5.15%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=0,j=0,k=0;
-        vector<int> v(m+n);
-        while(i<m && j<n){
-            if(nums1[i]<=nums2[j]){
-                v[k] = nums1[i++];
+        int i=m-1,j=n-1,k=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>=nums2[j]){         // from back so decreasing order
+                nums1[k] = nums1[i--];  
             }
             else{
-                v[k] = nums2[j++];
+                nums1[k] = nums2[j--];
             }
-            k++;
+            k--;
         }
-        while(i<m){
-            v[k++] = nums1[i++];
+        while(j>=0){
+            nums1[k--] = nums2[j--];
         }
-        while(j<n){
-            v[k++] = nums2[j++];
-        }
-        nums1 = v;
+        // while(i>=0){
+        //     nums1[k--] = nums1[i--];    // (i==k) put the same element at the same place -> no need.
+        // }
     }
     
 };
