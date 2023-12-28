fa81d92bd704b9f9a94464202d388df613a5d15b
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2023-12-28 09:10:00 +0530

    Time: 13 ms (74.31%), Space: 9 MB (97.73%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        int a[26],b[26];
+        int a[26];
         memset(a,0,sizeof(a));
-        memset(b,0,sizeof(b));
-        for(char ch: ransomNote) a[ch-'a']++;
-        for(char ch: magazine) b[ch-'a']++;
-        for(int i=0;i<26;i++){
-            if(b[i]<a[i]) return false;
+        for(char ch: magazine) a[ch-'a']++;
+        
+        for(char ch: ransomNote){
+            if(a[ch-'a']==0) return false;
+            a[ch-'a']--;
         }
         return true;
     }
