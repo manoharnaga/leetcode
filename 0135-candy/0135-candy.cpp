d0f4a81ec28f41bf0826d6f26e0c003bd5a46fea
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-01-04 18:07:32 +0530

    Time: 14 ms (82.59%), Space: 18.1 MB (41.78%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -10,16 +10,15 @@ public:
 		 if(ratings[i]>ratings[i-1])
 			 num[i]=num[i-1]+1;
 	 }
-	 for (int i= size-1; i>0 ; i--)
+	 for (int i= size-2; i>=0 ; i--)
 	 {
-		 if(ratings[i-1]>ratings[i])
-			 num[i-1]=max(num[i]+1,num[i-1]);
+		 if(ratings[i]>ratings[i+1])
+			 num[i]=max(num[i+1]+1,num[i]);
 	 }
 	 int result=0;
 	 for (int i = 0; i < size; i++)
 	 {
 		 result+=num[i];
-		// cout<<num[i]<<" ";
 	 }
 	 return result;
     }
