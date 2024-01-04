9fd9b1f0d73ef3b309ee00d01ff7e7160c0b833a
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-01-04 12:35:54 +0530

    Time: 24 ms (52.23%), Space: 25.4 MB (52.06%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -8,10 +8,9 @@ public:
         }
     }
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        k = k%n;
-        rev(nums,0,n-k-1);
-        rev(nums,n-k,n-1);
-        rev(nums,0,n-1);
+        k=k%nums.size();
+        reverse(nums.begin(),nums.begin()+(nums.size()-k));
+        reverse(nums.begin()+(nums.size()-k),nums.end());
+        reverse(nums.begin(),nums.end());
     }
 };
