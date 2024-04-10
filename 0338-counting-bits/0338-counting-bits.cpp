ae10dffddf1fc5be1c1d1403ad6f23855a4cca34
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-04-11 00:05:42 +0530

    Time: 0 ms (100.00%), Space: 9 MB (80.74%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        std::ios::sync_with_stdio(0);
-        std::cin.tie(0);
-        std::cout.tie(0);
         vector<int> ans(n+1,0);
         ans[0] = 0;
         for(int i=0;i<=n;i++){
