459ddc588d9178ff85e67b3080c48a8e2266b557
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-04-10 13:34:53 +0530

    Time: 10 ms (33.83%), Space: 12.6 MB (48.66%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void rec(vector<int>& a,vector<int>& v,int ind,int x){
+        if(x<0) return;
+        if(x==0){
+            ans.push_back(v);
+            return;
+        }
+        if(ind>=a.size()) return;
+        v.push_back(a[ind]);
+        rec(a,v,ind,x-a[ind]);
+        v.pop_back();
+        rec(a,v,ind+1,x);
+    }
+    vector<vector<int>> combinationSum(vector<int>& a, int x) {
+        sort(a.begin(),a.end());
+        vector<int> v;
+        rec(a,v,0,x);
+        return ans;
+    }
+};
