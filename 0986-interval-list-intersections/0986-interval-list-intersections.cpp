f8bbc7ef28430d04bc966efbd71d5490479f07d7
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-04-13 22:47:13 +0530

    Time: 26 ms (43.61%), Space: 22.2 MB (91.90%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.cpp
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -6,30 +6,11 @@ public:
         int i=0,j=0;
         vector<vector<int>> ans;
         while(i<n && j<m){
-            if(a[i][1]<b[j][0]){
-                i++;
-            }
-            else if(b[j][1]<a[i][0]){
-                j++;
-            }
-            else{
-                // cout<<i<<j<<"\n";
-                if(a[i][0]<b[j][0]){
-                    ans.push_back({b[j][0],min(a[i][1],b[j][1])});
-                }
-                else{
-                    ans.push_back({a[i][0],min(b[j][1],a[i][1])});
-                }
-                if(a[i][1]<b[j][1]){
-                    i++;
-                }
-                else if(a[i][1]==b[j][1]){
-                    i++;j++;
-                }
-                else{
-                    j++;
-                }
-            }
+            int u = max(a[i][0],b[j][0]);
+            int v = min(a[i][1],b[j][1]);
+            if(u<=v) ans.push_back({u,v}); // not my code
+            if(a[i][1]<b[j][1]) i++;
+            else j++;
         }
         return ans;
     }
