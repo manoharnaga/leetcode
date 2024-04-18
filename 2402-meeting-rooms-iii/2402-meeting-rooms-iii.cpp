cece285a6c3a7d7d9f170098bed2be6fbb284c1f
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-04-19 00:06:52 +0530

    Time: 332 ms (33.20%), Space: 111.7 MB (30.81%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
@@ -1,42 +1,36 @@
 class Solution {
 public:
-    int mostBooked(int n, vector<vector<int>>& meetings) {
-        vector<long long> roomAvailabilityTime(n, 0);
-        vector<int> meetingCount(n, 0);
-        sort(meetings.begin(), meetings.end());
-
-        for (auto& meeting: meetings) {
-            int start = meeting[0], end = meeting[1];
-            long long minRoomAvailabilityTime = LLONG_MAX;
-            int minAvailableTimeRoom = 0;
-            bool foundUnusedRoom = false;
-
-            for (int i = 0; i < n; i++) {
-                if (roomAvailabilityTime[i] <= start) {
-                    foundUnusedRoom = true;
-                    meetingCount[i]++;
-                    roomAvailabilityTime[i] = end;
-                    break;
-                }
-
-                if (minRoomAvailabilityTime > roomAvailabilityTime[i]) {
-                    minRoomAvailabilityTime = roomAvailabilityTime[i];
-                    minAvailableTimeRoom = i;
-                }
+    typedef long long ll;
+    int mostBooked(int n, vector<vector<int>>& a) {
+        sort(a.begin(),a.end());
+        int m = a.size();
+        priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>> pq;
+        priority_queue<int,vector<int>,greater<int>> q;
+        for(int i=0;i<n;i++) q.push(i);
+        
+        vector<int> v(m,1);
+        
+        for(auto x: a){
+            int start = x[0],end = x[1];
+            while(!pq.empty() && pq.top().first<=start){
+                q.push(pq.top().second);
+                pq.pop();
             }
-
-            if (!foundUnusedRoom) {
-                roomAvailabilityTime[minAvailableTimeRoom] += end - start;
-                meetingCount[minAvailableTimeRoom]++;
+            if(!q.empty()){
+                v[q.top()]++;
+                pq.push({end,q.top()});
+                q.pop();
+            }
+            else{
+                v[pq.top().second]++;
+                ll val = pq.top().first + end-start;
+                pq.push({val,pq.top().second});
+                pq.pop();
             }
         }
-        int maxMeetingCount = 0, maxMeetingCountRoom = 0;
-        for (int i = 0; i < n; i++) {
-            if (meetingCount[i] > maxMeetingCount) {
-                maxMeetingCount = meetingCount[i];
-                maxMeetingCountRoom = i;
-            }
-        }
-        return maxMeetingCountRoom;
+        // for(int val: v) cout<<val<<" ";
+        // cout<<"\n";
+        int ind = max_element(v.begin(),v.end())-v.begin();
+        return ind;
     }
 };
