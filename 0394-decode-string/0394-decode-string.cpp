fdeae0d134abf59ab1f52083da4a2c742ba90dee
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-04-19 10:49:12 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (83.22%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
+    string decodeString(const string& s, int& i) {
+        string res;
+        
+        while (i < s.length() && s[i] != ']') {
+            if (!isdigit(s[i]))
+                res += s[i++];
+            else {
+                int n = 0;
+                while (i < s.length() && isdigit(s[i]))
+                    n = n * 10 + s[i++] - '0';
+                    
+                i++; // '['
+                string t = decodeString(s, i);
+                i++; // ']'
+                
+                while (n--)
+                    res += t;
+            }
+        }
+        
+        return res;
+    }
+
     string decodeString(string s) {
-        stack<char> st;
-        for(char ch: s){
-            if(ch==']'){
-                string temp;
-                while(st.top()!='['){
-                    temp+=st.top();
-                    st.pop();
-                }
-                st.pop();
-                string x;
-                while(!st.empty() && st.top()>='0' && st.top()<='9'){
-                    x+=st.top();
-                    st.pop();
-                }
-                reverse(x.begin(),x.end());
-                int val = stoi(x);
-                for(int i=0;i<val;i++){
-                    for(int j=temp.size()-1;j>=0;j--){
-                        st.push(temp[j]);
-                    }
-                }
-            }
-            else{
-                st.push(ch);
-            }
-        }
-        string ans;
-        while(!st.empty()){
-            ans+=st.top();
-            st.pop();
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        int i = 0;
+        return decodeString(s, i);
     }
 };
