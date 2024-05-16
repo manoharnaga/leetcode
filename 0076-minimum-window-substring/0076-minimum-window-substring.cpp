0a9651b64994463359a9c45032eeb922bcd613b9
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-05-16 22:44:33 +0530

    Time: 11 ms (71.78%), Space: 9.7 MB (75.87%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,46 +1,27 @@
 class Solution {
 public:
-    int a[26],b[26],c[26],d[26];
     
-    int check(){
-        for(int i=0;i<26;i++){
-            if(a[i]>b[i]) return 0;
-        }
-        for(int i=0;i<26;i++){
-            if(c[i]>d[i]) return 0;
-        }
-        return 1;
-    }
     string minWindow(string s, string t) {
-        int n = s.size();
-        int m = t.size();
-        int l = 0,e = 0;
-        for(int i=0;i<26;i++){
-            a[i] = b[i] = c[i] = d[i] = 0;
+        vector<int> map(128,0);
+        for (char c : t) {
+            map[c]++;
         }
-        for(char ch: t){
-            if(ch>='a' && ch<='z') a[ch-'a']++;
-            else                   c[ch-'A']++;
-        }
-        int pL = 0,pR = n;
-        while(e<n){
-            char ch = s[e];
-            if(ch>='a' && ch<='z') b[ch-'a']++;
-            else                   d[ch-'A']++;
-            while(l<=e && check()){
-                if((e-l+1)<(pR-pL+1)){
-                    pL = l;
-                    pR = e;
-                }
-                ch = s[l];
-                if(ch>='a' && ch<='z') b[ch-'a']--;
-                else                   d[ch-'A']--;
-                l++;
+
+        int counter = t.size(), begin = 0, end = 0, d = INT_MAX, head = 0;
+        while (end < s.size()){
+            if (map[s[end++]]-- > 0) {
+                counter--;
             }
-            e++;
+            while (counter == 0) {
+                if (end - begin < d) {
+                    head = begin;
+                    d = end - head;
+                }
+                if (map[s[begin++]]++ == 0) {
+                    counter++;
+                }
+            }  
         }
-        if(pR==n) return "";
-        string ans = s.substr(pL,pR-pL+1);
-        return ans;
+        return d == INT_MAX ? "" : s.substr(head, d);
     }
 };
