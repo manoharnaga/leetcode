319266c8b6f9716c2fb2390fa4930eec87150eec
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-07-05 16:21:48 +0530

    Time: 73 ms (5.53%), Space: 17.1 MB (5.53%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        string ans="";
-        for(char ch: num){
-            while(!ans.empty() && k && ans.back()>ch){
-                ans.pop_back();
+        priority_queue<pair<int,int>> pq;
+        for(int i=0;i<num.size();i++){
+            int val = num[i]-'0';
+            while(!pq.empty() && pq.top().first>val && k>0){
+                pq.pop();
                 k--;
             }
-            if(ans.empty() && ch=='0') continue;
-            ans.push_back(ch);
+            pq.push({val,i});
         }
-        while(!ans.empty() && k--){
-            ans.pop_back();
+        // while(!pq.empty()){
+        //     cout<<pq.top().first<<" "<<pq.top().second<<"\n";
+        //     pq.pop();
+        // }
+        while(!pq.empty() && k>0){
+            pq.pop();
+            k--;
         }
+        priority_queue<pair<int,int>> temp;
+        while(!pq.empty()){
+            temp.push({pq.top().second,pq.top().first});
+            pq.pop();
+        }
+        string ans;
+        while(!temp.empty()){
+            ans+=to_string(temp.top().second);
+            temp.pop();
+        }
+        while(!ans.empty() && ans.back()=='0') ans.pop_back();
+        reverse(ans.begin(),ans.end());
         if(ans.empty()) return "0";
         return ans;
     }
