1b3368b704aa5d6118abe3351b0c81348563b04e
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-07-15 21:16:02 +0530

    Time: 2269 ms (5.03%), Space: 565.2 MB (5.68%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& a) {
         int n = a.size();
-        sort(a.begin(),a.end(),[](const auto& x,const auto& y){
-            if(x[1]==y[1]){
-                return x[0]<y[0];
-            }
+        auto cmp = [&](auto x,auto y){
+            if(x[1]==y[1]) return x[0]<y[0];
             return x[1]<y[1];
-        });
+        };
+        sort(a.begin(),a.end(),cmp);
+        int end = a[0][1];
         int cnt = 0;
-        int prev = a[0][1];
-        // cout<<prev<<"x\n";
         for(int i=1;i<n;i++){
-            // cout<<a[i][0]<<"yz\n";
-            if(prev > a[i][0]){
+            if(a[i][0]<end){
                 cnt++;
             }
             else{
-                prev = a[i][1];
+                end = a[i][1];
             }
         }
         return cnt;
