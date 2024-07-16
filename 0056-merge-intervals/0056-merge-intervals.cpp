3e62a975d2c162cb88e580eabc204854c56c37fe
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-07-16 19:28:21 +0530

    Time: 22 ms (68.09%), Space: 24 MB (10.84%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& a) {
-        sort(a.begin(),a.end());
-        vector<vector<int>> ans;
-        int start = a[0][0],end = a[0][1];
-        for(int i=1;i<a.size();i++){
-            if(end>=a[i][0]){
-                end = max(a[i][1],end);
+        sort(a.begin(),a.end(),[](const auto& x,const auto& y){
+            if(x[0]==y[0]) return x[1]>y[1];
+            return x[0]<y[0];
+        });
+        int ind = 0;
+        int n = a.size();
+        
+        for(int i=1;i<n;i++){
+            if(a[i][0]<=a[ind][1]){
+                a[ind][1] = max(a[ind][1],a[i][1]);
+                if(a[i][1]>a[ind][1]){
+                    ind = i;
+                }
+                a[i][0] = a[i][1] = -1;
             }
             else{
-                ans.push_back({start,end});
-                start = a[i][0];
-                end = a[i][1];
+                ind = i;
             }
         }
-        ans.push_back({start,end});
+        vector<vector<int>> ans;
+        for(auto it: a){
+            if(it[0]==-1 && it[1]==-1) continue;
+            ans.push_back(it);
+        }
         return ans;
     }
 };
