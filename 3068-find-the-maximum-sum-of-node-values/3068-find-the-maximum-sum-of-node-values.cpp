c1ec3d95066c855954be00f1e979708d94383281
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-07-21 16:42:26 +0530

    Time: 195 ms (50.61%), Space: 128.5 MB (38.50%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     typedef long long ll;
     ll dp[20010][2];
-    ll rec(vector<int>& a,int isEven,int idx,int k){
-        if(idx == a.size()){
-            return (isEven==1 ? 0 : INT_MIN);
+    ll rec(vector<int>& a,int n,int isEven,int k){
+        if(n<0){
+            return (isEven==1 ? 0: INT_MIN);
         }
-        if(dp[idx][isEven]!=-1) return dp[idx][isEven];
+        if(dp[n][isEven]!=-1) return dp[n][isEven];
         
-        ll notake = a[idx]+rec(a,isEven,idx+1,k);
-        ll take = (a[idx]^k) + rec(a,isEven^1,idx+1,k);
-        return dp[idx][isEven] = max(notake,take);
+        ll take = (a[n]^k) + rec(a,n-1,isEven^1,k);
+        ll notake = a[n] + rec(a,n-1,isEven,k);
+        // cout<<n<<" "<<a[n]<<" "<<take<<" "<<notake<<"\n"
+        return dp[n][isEven] = max(take,notake);
     }
     long long maximumValueSum(vector<int>& a, int k, vector<vector<int>>& edges) {
         int n = a.size();
         for(int i=0;i<n;i++){
-            for(int j=0;j<2;j++)
+            for(int j=0;j<2;j++){
                 dp[i][j] = -1;
+            }
         }
-        ll ans = rec(a,1,0,k);
-        return ans;
+        // cout<<dp[n-1][0]<<"\n";
+        return rec(a,n-1,1,k);
     }
 };
