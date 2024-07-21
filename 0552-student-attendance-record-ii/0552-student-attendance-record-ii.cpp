88a35c1171529ecf2e28d6b8ca3d69ac1021e983
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-07-21 17:33:58 +0530

    Time: 164 ms (70.21%), Space: 20.5 MB (61.78%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -1,41 +1,31 @@
 class Solution {
-    int MOD = 1000000007;
-    // Cache to store sub-problem results.
-    vector<vector<vector<int>>> memo;
-
 public:
-    // Recursive function to return the count of combinations of length 'n' eligible for the award.
-    int eligibleCombinations(int n, int totalAbsences, int consecutiveLates) {
-        // If the combination has become not eligible for the award,
-        // then we will not count any combinations that can be made using it.
-        if (totalAbsences >= 2 or consecutiveLates >= 3) {
-            return 0;
-        }
-        // If we have generated a combination of length 'n' we will count it.
-        if (n == 0) {
-            return 1;
-        }
-        // If we have already seen this sub-problem earlier, we return the stored result.
-        if (memo[n][totalAbsences][consecutiveLates] != -1) {
-            return memo[n][totalAbsences][consecutiveLates];
-        }
-
-        int count = 0;
-        // We choose 'P' for the current position.
-        count = eligibleCombinations(n - 1, totalAbsences, 0);
-        // We choose 'A' for the current position.
-        count = (count + eligibleCombinations(n - 1, totalAbsences + 1, 0)) % MOD;
-        // We choose 'L' for the current position.
-        count = (count + eligibleCombinations(n - 1, totalAbsences, consecutiveLates + 1)) % MOD;
-
-        // Return and store the current sub-problem result in the cache.
-        return memo[n][totalAbsences][consecutiveLates] = count;
+    typedef long long ll;
+    #define MOD 1000000007
+    ll dp[100010][2][3];
+    ll rec(int n,int abscnt,int latecnt){
+        // cout<<"hi\n";
+        if(abscnt>1 || latecnt>2) return 0ll;
+        if(n<0) return 1ll;
+        if(dp[n][abscnt][latecnt]!=-1) return dp[n][abscnt][latecnt];
+        
+        ll cnt = rec(n-1,abscnt,0);
+        cnt += rec(n-1,abscnt+1,0);
+        cnt %= MOD;
+        cnt += rec(n-1,abscnt,latecnt+1);
+        cnt %= MOD;
+        // cout<<cnt<<"\n";
+        return dp[n][abscnt][latecnt] = cnt;
     }
-
+    
     int checkRecord(int n) {
-        // Initialize the cache.
-        memo = vector<vector<vector<int>>>(n + 1, vector<vector<int>>(2, vector<int>(3, -1)));
-        // Return count of combinations of length 'n' eligible for the award.
-        return eligibleCombinations(n, 0, 0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2;j++){
+                for(int k=0;k<3;k++){
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
+        return rec(n-1,0,0);
     }
 };
