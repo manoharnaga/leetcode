3492bf381e6ddf3b2bc71702a82791f0b1d3cb43
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-01 16:29:52 +0530

    Time: 4 ms (95.53%), Space: 16.7 MB (53.72%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,22 +3,32 @@ public:
     void setZeroes(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        vector<int> row(n),col(m);
-
+        int col0 = 1;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(!mat[i][j]){
-                   row[i] = 1;
-                   col[j] = 1;
+                   mat[i][0] = 0;
+                   if(j!=0)
+                        mat[0][j] = 0;
+                   else
+                       col0 = 0;
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(row[i] || col[j]){
-                    mat[i][j] = 0;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(mat[i][j]!=0){
+                    if(mat[i][0]==0 || mat[0][j]==0){
+                        mat[i][j] = 0;
+                    }
                 }
             }
+        }
+        if(mat[0][0]==0){
+            for(int j=0;j<m;j++) mat[0][j] = 0;
+        }
+        if(col0==0){
+            for(int i=0;i<n;i++) mat[i][0] = 0;
         }
     }
 };
