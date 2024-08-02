daf0e994a3b054ddd676b1a09282745b22888aba
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-02 22:20:31 +0530

    Time: 100 ms (5.07%), Space: 71.8 MB (5.01%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        return maxSubArray(nums, 0, size(nums)-1);
+    int rec(vector<int>& a,int l,int r){
+        if(l>r) return INT_MIN;
+        int mid = (l+r)/2;
+        int lval=0,rval = 0;
+        for(int i=mid-1,cursum=0;i>=l;i--){
+            cursum+=a[i];
+            lval = max(lval,cursum);
+        }
+        for(int i=mid+1,cursum=0;i<=r;i++){
+            cursum+=a[i];
+            rval = max(rval,cursum);
+        }
+        return max({rec(a,l,mid-1),rec(a,mid+1,r),lval+a[mid]+rval});
     }
-    int maxSubArray(vector<int>& A, int L, int R){
-        if(L > R) return INT_MIN;
-        int mid = (L + R) / 2, leftSum = 0, rightSum = 0;
-        // leftSum = max subarray sum in [L, mid-1] and starting from mid-1
-        for(int i = mid-1, curSum = 0; i >= L; i--)
-            curSum += A[i],
-            leftSum=max(leftSum, curSum);
-        // rightSum = max subarray sum in [mid+1, R] and starting from mid+1
-        for(int i = mid+1, curSum = 0; i <= R; i++)
-            curSum += A[i],
-            rightSum = max(rightSum, curSum);        
-		// return max of 3 cases 
-        return max({ maxSubArray(A, L, mid-1), maxSubArray(A, mid+1, R), leftSum + A[mid] + rightSum });
-    }	
+    int maxSubArray(vector<int>& a) {
+        return rec(a,0,a.size()-1);
+    }
 };
