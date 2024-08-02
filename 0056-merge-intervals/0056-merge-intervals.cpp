2a9e79bfeedc60c57e9f901f07258c927494c98f
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-02 22:31:46 +0530

    Time: 27 ms (34.03%), Space: 23 MB (40.60%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,26 +5,20 @@ public:
             if(x[0]==y[0]) return x[1]>y[1];
             return x[0]<y[0];
         });
-        int ind = 0;
         int n = a.size();
-        
-        for(int i=1;i<n;i++){
-            if(a[i][0]<=a[ind][1]){
-                a[ind][1] = max(a[ind][1],a[i][1]);
-                if(a[i][1]>a[ind][1]){
-                    ind = i;
-                }
-                a[i][0] = a[i][1] = -1;
+        vector<vector<int>> ans;
+        for(int i=0;i<n-1;i++){
+            int l = max(a[i][0],a[i+1][0]);
+            int r = min(a[i][1],a[i+1][1]);
+            if(l<=r){
+                a[i+1][0] = min(a[i][0],a[i+1][0]);
+                a[i+1][1] = max(a[i][1],a[i+1][1]);
             }
             else{
-                ind = i;
+                ans.push_back(a[i]);
             }
         }
-        vector<vector<int>> ans;
-        for(auto it: a){
-            if(it[0]==-1 && it[1]==-1) continue;
-            ans.push_back(it);
-        }
+        ans.push_back(a[n-1]);
         return ans;
     }
 };
