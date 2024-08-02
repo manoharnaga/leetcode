f3aa6201ccb12bf8caf1430e35b93ba91895bda9
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-02 22:16:57 +0530

    Time: 108 ms (5.07%), Space: 71.8 MB (5.01%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& a) {
-        int ans = INT_MIN;
-        int maxsofar = 0;
-        for(int i=0;i<a.size();i++){
-            maxsofar += a[i];
-            ans = max(maxsofar,ans);
-            maxsofar = max(maxsofar,0);
-        }
-        return ans;
+    int maxSubArray(vector<int>& nums) {
+        return maxSubArray(nums, 0, size(nums)-1);
     }
+    int maxSubArray(vector<int>& A, int L, int R){
+        if(L > R) return INT_MIN;
+        int mid = (L + R) / 2, leftSum = 0, rightSum = 0;
+        // leftSum = max subarray sum in [L, mid-1] and starting from mid-1
+        for(int i = mid-1, curSum = 0; i >= L; i--)
+            curSum += A[i],
+            leftSum=max(leftSum, curSum);
+        // rightSum = max subarray sum in [mid+1, R] and starting from mid+1
+        for(int i = mid+1, curSum = 0; i <= R; i++)
+            curSum += A[i],
+            rightSum = max(rightSum, curSum);        
+		// return max of 3 cases 
+        return max({ maxSubArray(A, L, mid-1), maxSubArray(A, mid+1, R), leftSum + A[mid] + rightSum });
+    }	
 };
