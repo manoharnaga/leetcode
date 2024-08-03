b017af09f0a5304b44f468ee786a0c55e7b07e42
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-03 10:52:58 +0530

    Time: 18 ms (47.83%), Space: 15.9 MB (5.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,15 +3,11 @@ public:
     vector<int> twoSum(vector<int>& a, int target) {
         map<int,int> mp;
         int n = a.size();
-        vector<pair<int,int>> v;
-        for(int i=0;i<n;i++) v.push_back({a[i],i});
-        sort(v.begin(),v.end());
-        int low=0,high=n-1;
-        while(low<high){
-            int sum = v[low].first+v[high].first;
-            if(sum>target) high--;
-            else if(sum<target) low++;
-            else return {v[low].second,v[high].second};
+        for(int i=0;i<n;i++){
+            if(mp[(target-a[i])]){
+                return {mp[target-a[i]]-1,i};
+            }
+            mp[a[i]] = i+1;
         }
         return {-1,-1};
     }
