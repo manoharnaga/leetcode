fe6ee69d8949b9e887f76006bce913a8dfae4d37
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-07 16:45:32 +0530

    Time: 352 ms (68.25%), Space: 141.7 MB (65.92%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -2,33 +2,22 @@ class Solution {
 public:
     typedef long long ll;
     ll ans;
-    int bin(vector<ll>& a,int x){
-        int low = 0,high = a.size()-1;
-        int ind = -1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(a[mid]>=x){
-                high = mid-1;
-            }
-            else{
-                ind = mid;
-                low = mid+1;
-            }
-        }
-        return ind;
-    }
     void merge(vector<int>& a,int low,int mid,int high){
         vector<int> L(mid-low+1),R(high-mid);
         for(int i=low;i<=mid;i++) L[i-low]=a[i];
         for(int i=mid+1;i<=high;i++) R[i-mid-1]=a[i];
         int n = L.size(),m = R.size();
         int i=0,j=0,k=low;
-        vector<ll> v(m);
-        for(int i=0;i<m;i++) v[i]=(2ll*R[i]);
-        for(int j=0;j<n;j++){
-            int ind = bin(v,L[j]);
-            if(ind!=-1) ans += (ind+1);
+        while(i<n && j<m){
+            if(L[i]>(ll)2 * R[j]){
+                ans += (n-i);
+                j++;
+            }
+            else{
+                i++;
+            }
         }
+        i=j=0;
         while(i<n && j<m){
             if(L[i]<=R[j]){
                 a[k] = L[i++];
