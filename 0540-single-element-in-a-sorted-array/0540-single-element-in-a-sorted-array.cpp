1c2e0a4cca7df79495df659ff5639573da035b1e
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-10 17:34:42 +0530

    Time: 22 ms (31.02%), Space: 24.9 MB (40.08%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -3,19 +3,23 @@ public:
     int singleNonDuplicate(vector<int>& a) {
         int low = 0,high = a.size()-1;
         int n = a.size();
-        while(low<=high){
+        while(low<high){
             int mid = (low+high)/2;
-            int cnt = mid+1;
-            int cnt2 = n-1-mid;
-            cout<<low<<" "<<mid<<" "<<high<<"\n";
-            if((mid-1)>=0 && a[mid-1]==a[mid]){
-                cout<<cnt<<endl; 
-                if(cnt%2) high = mid-1;
-                else      low = mid+1;
+            // cout<<low<<" "<<mid<<" "<<high<<"\n";
+            if((high-low+1)==3){
+                if(a[low]==a[low+1]) return a[high];
+                else                 return a[low];
             }
-            else if((mid+1)<n && a[mid]==a[mid+1]){
-                if(cnt2%2) high = mid-1;
+            int cnt1=0,cnt2=0;
+            if((mid-1)>=0 && a[mid-1]==a[mid]){
+                cnt1 = mid-1-low;
+                if(cnt1%2) high = mid-2;
                 else       low = mid+1;
+            }
+            else if((mid+1)<n && a[mid+1]==a[mid]){
+                cnt2 = high-mid-1;
+                if(cnt2%2) low = mid+2;
+                else       high = mid-1;
             }
             else{
                 return a[mid];
