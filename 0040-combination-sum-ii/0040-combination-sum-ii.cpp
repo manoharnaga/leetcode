391f063f0764cc1390409cd7d5aab07958cf7a3d
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-10 09:27:27 +0530

    Time: 0 ms (100.00%), Space: 16.1 MB (15.80%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    set<vector<int>> st;
-    void rec(vector<int>& a, int n, vector<int>& v, int sum){
+    vector<vector<int>> ans;
+    void rec(vector<int>& a,vector<int>& v,int n,int f,int sum){
         if(sum==0){
-            st.insert(v);
+            ans.push_back(v);
             return;
         }
-        if(n<0 || sum<0) return;
+        if(sum<0 || n<0) return;
+        rec(a,v,n-1,0,sum);      // no take
+        
+        if((n+1)<a.size() && a[n+1]==a[n] && !f) return;
         v.push_back(a[n]);
-        rec(a,n-1,v,sum-a[n]);
+        rec(a,v,n-1,1,sum-a[n]); // take
         v.pop_back();
-        while((n-1)>=0 && a[n-1]==a[n]) n--;
-        rec(a,n-1,v,sum);
     }
     vector<vector<int>> combinationSum2(vector<int>& a, int target) {
-        st.clear();
-        sort(a.begin(),a.end());
-        // reverse(a.begin(),a.end());
         int n = a.size();
+        // dp = vector<vector<int>> (target+1,vector<int>(n,-1));
+        sort(a.begin(),a.end());
+        reverse(a.begin(),a.end());
         vector<int> v;
-        rec(a,n-1,v,target);
-        vector<vector<int>> ans;
-        for(auto it: st) ans.push_back(it);
+        int f = 0;
+        rec(a,v,n-1,f,target);
         return ans;
     }
 };
