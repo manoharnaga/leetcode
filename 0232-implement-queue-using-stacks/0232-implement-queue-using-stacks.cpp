553fcef24a26e9d3f41405b8c038064e538a5d87
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-11 18:42:13 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (49.68%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -10,34 +10,30 @@ public:
     }
     
     int pop() {
-        while(!s1.empty()){
-            s2.push(s1.top());
-            s1.pop();
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
         }
         int front = s2.top();
         s2.pop();
-        while(!s2.empty()){
-            s1.push(s2.top());
-            s2.pop();
-        }
         return front;
     }
     
     int peek() {
-        while(!s1.empty()){
-            s2.push(s1.top());
-            s1.pop();
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
         }
         int front = s2.top();
-        while(!s2.empty()){
-            s1.push(s2.top());
-            s2.pop();
-        }
         return front;
     }
     
     bool empty() {
-        return s1.empty();
+        return s1.empty() && s2.empty();
     }
 };
 
