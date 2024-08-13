01c4e8099e8839338aa4197fca72765fed7f2efc
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-13 12:30:40 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (16.12%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int m = haystack.size();
-        int n = needle.size();
-        for(int i=0;i+n-1<m;i++){
-            int f=0;
-            for(int j=0;j<n;j++){
-                // cout<<haystack[i]<<needle[i+j]<<" ";
-                if(needle[j]!=haystack[i+j]){
-                    f=1;
-                    break;
-                }
-            }
-            // cout<<"\n";
-            if(!f) return i;
+    vector<int> prefix_function(string s) {
+        int n = (int)s.length();
+        vector<int> pi(n);
+        for (int i = 1; i < n; i++) {
+            int j = pi[i-1];
+            while (j > 0 && s[i] != s[j])
+                j = pi[j-1];
+            if (s[i] == s[j])
+                j++;
+            pi[i] = j;
         }
-        // cout<<"\n";
+        return pi;
+    }
+
+    int strStr(string haystack, string needle) {
+        string total = needle;
+        total += "#";
+        total += haystack;
+        vector<int> pi = prefix_function(total);
+        int n = needle.size();
+        for(int i=0;i<total.size();i++){
+            if(pi[i]==n){
+                return (i-(n+1)-(n-1));
+            }
+        }
         return -1;
     }
 };
