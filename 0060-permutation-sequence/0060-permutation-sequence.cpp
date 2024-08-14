c3106c766c8d446ef0b3c800a90bb54f80e3cbf6
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-14 14:59:11 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (26.26%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -3,18 +3,16 @@ public:
     string getPermutation(int n, int k) {
         string ans = "";
         int fact = 1;
-        vector<int> v(n-1);
-        for(int i=1;i<n;i++){
-            fact*=i;
-            v[i-1]=i;
-        }
-        v.push_back(n);
+        for(int i=1;i<n;i++) fact*=i;
+        vector<int> v;
+        for(int i=1;i<=n;i++) v.push_back(i);
         k = k-1;
         while(1){
-            int idx = (k/fact);
+            int idx = k/fact;
             ans += to_string(v[idx]);
             v.erase(v.begin()+idx);
             if(v.empty()) break;
+            
             k = k%fact;
             fact = fact/v.size();
         }
