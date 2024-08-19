7ed22bc30b497143d8c1f1174ba0dcfb5baf5869
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-20 00:40:08 +0530

    Time: 13 ms (53.34%), Space: 16.4 MB (39.66%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,24 +7,43 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {    
-    TreeNode* ans;
-    vector<int> rec(TreeNode* root, TreeNode* p, TreeNode* q){
-        if(!root) return {0,0};
-        vector<int> x = rec(root->left,p,q);
-        if(ans) return x;
-        vector<int> y = rec(root->right,p,q);
-        x[0] = (root->val==p->val) || x[0] || y[0];
-        x[1] = (root->val==q->val) || x[1] || y[1];
-        if(!ans && x[0] && x[1]){
-            ans = root;
-        }
-        return x;
-    }
+class Solution {
 public:
+    TreeNode* found;
+    TreeNode* rec(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(!root) return NULL;
+        
+        TreeNode* l = rec(root->left,p,q);
+        TreeNode* r = rec(root->right,p,q);
+        
+        if(found){
+            return found;
+        }
+        
+        if(root==p || root==q){
+            if(l==p || l==q || r==p || r==q){
+                found = root; // p or q is ancestor
+                return found;
+            }
+            else{
+                return root;
+            }
+        }
+        
+        if((l==p && r==q) || (l==q && r==p)){
+            found = root; // p,q have diff ancestor
+            return found;
+        }
+        
+        if(l==p || l==q) return l;
+        if(r==p || r==q) return r;
+        
+        return NULL;
+    }
+    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        ans = nullptr;
+        found = NULL;
         rec(root,p,q);
-        return ans;
+        return found;
     }
 };
