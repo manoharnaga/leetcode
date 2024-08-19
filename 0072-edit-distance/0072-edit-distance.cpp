f5d7c7afb875b5a8bf5835b7c295c6f99333b373
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-19 17:12:30 +0530

    Time: 23 ms (22.36%), Space: 13.4 MB (21.01%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int rec(string& x,string& y,int n,int m,vector<vector<int>>& dp){
-        if(n==0) return dp[n][m] = m;
-        if(m==0) return dp[n][m] = n;
+        if(n<0 || m<0) return max(n,m)+1;
         
         if(dp[n][m]!=-1) return dp[n][m];
         
-        int f1 = !(x[n-1]==y[m-1])+rec(x,y,n-1,m-1,dp);
+        int f1 = !(x[n]==y[m])+rec(x,y,n-1,m-1,dp);
         int f2 = 1+rec(x,y,n-1,m,dp);
         int f3 = 1+rec(x,y,n,m-1,dp);
         return dp[n][m] = min({f1,f2,f3});
@@ -16,6 +15,6 @@ public:
         int n = word1.size();
         int m = word2.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return rec(word1,word2,n,m,dp);
+        return rec(word1,word2,n-1,m-1,dp);
     }
 };
