c59357bb3675312c9dcadc141aa770881ae956c1
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-22 17:20:11 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (18.94%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,15 +11,41 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int>& v){
-        if(!root) return;
-        inorder(root->left,v);
-        v.push_back(root->val);
-        inorder(root->right,v);
-    }
+    // void inorder(TreeNode* root,vector<int>& v){
+    //     if(!root) return;
+    //     inorder(root->left,v);
+    //     v.push_back(root->val);
+    //     inorder(root->right,v);
+    // }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        inorder(root,ans);
+        
+        while(root){
+            if(!root->left){
+                ans.push_back(root->val);
+                root = root->right;
+            }
+            else{
+                TreeNode* rightmost = root->left;
+                int f = 0;
+                while(rightmost->right){
+                    if(rightmost && rightmost->right==root){
+                        rightmost->right = nullptr;
+                        ans.push_back(root->val);
+                        f=1;
+                        break;
+                    }
+                    rightmost = rightmost->right;
+                }
+                if(!f){
+                    rightmost->right = root;
+                    root = root->left;
+                } 
+                else{
+                    root = root->right;
+                }
+            }
+        }
         return ans;
     }
 };
