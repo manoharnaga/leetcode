6cf79d943f4f8e8a6e1b6984675d0e1cc981bcfa
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-22 21:24:45 +0530

    Time: 3 ms (84.45%), Space: 19.8 MB (40.45%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,93 +11,37 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-//         ListNode *p1,*c1,*n1;
-//         p1 = nullptr;
-//         c1 = l1;
-//         n1 = l1->next;
+        if(!l1) return l2;
+        if(!l2) return l1;
         
-//         ListNode *p2,*c2,*n2;
-//         p2 = nullptr;
-//         c2 = l2;
-//         n2 = l2->next;
+        ListNode* cur;
+        ListNode* head;
         
-//         while(c1 && c2){
-//             if(c1->val<=c2->val){
-//                 // p1->c1->c2->n1
-//                 // jump to n1
-//                 c1->next = c2;
-//                 c2->next = n1;
-//                 c1 = n1;
-//                 if(n1) n1 = n1->next;
-//             }
-//             else{
-//                 // p1->c2->c1->n1
-//                 // jump to n2
-//                 if(p1)  p1->next = c2;
-//                 c2->next = c1;
-//                 c2 = n2;
-//                 if(n2) n2 = n2->next;
-//             }
-//         }
-//         if(c2){
-//             p1->next = c2;
-//         }
-        // return l1;
-//         while(c2){
-            
-//         }
-        
-        ListNode *head, *cur;
-        if(l1 == nullptr) {
-            head = l2;
-            return head;
-        }
-        if(l2 == nullptr) {
-            head = l1; 
-            return head;
-        }
-        // cout<<(l1->val)<<" "<<(l2->val)<<endl;
-        if(l1->val < l2->val) {
-            head = l1; cur = l1;
+        if(l1->val<=l2->val){
+            head = l1;
             l1 = l1->next;
         }
-        else {
-            head = l2; cur = l2;
+        else{
+            head = l2;
             l2 = l2->next;
         }
-        
-        // while(cur){
-        //     cout<<cur->val<<" ";
-        //     cur = cur->next;
-        // }
-        // cout<<endl; 
+        cur = head;
         
         while(l1 && l2){
-            cout<<l1->val<<" "<<l2->val<<endl; 
-            if(l1->val < l2->val){
+            if(l1->val<=l2->val){
                 cur->next = l1;
                 l1 = l1->next;
-                // cur = cur->next;
-            } else {
+            }
+            else{
                 cur->next = l2;
                 l2 = l2->next;
             }
             cur = cur->next;
         }
         
-        while(l1){
-            cur -> next = l1; 
-            l1 = l1 -> next;
-            cur = cur -> next;
-        }
+        if(l1) cur->next = l1;
+        if(l2) cur->next = l2;
         
-        while(l2){
-            cur -> next = l2; 
-            l2 = l2 -> next;
-            cur = cur -> next;
-        }
-        
-        return head; 
-            
+        return head;
     }
 };
