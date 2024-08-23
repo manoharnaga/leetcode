d4804727c3f21a901d421d99466f68a2d61b9808
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-23 10:36:11 +0530

    Time: 3 ms (74.36%), Space: 11.7 MB (43.04%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<int> dp(1,1);
+        vector<int> v;
+        v.push_back(1);
         int i=0,j=0,k=0;
-        while(dp.size()<n){
-            dp.push_back(min({dp[i]*2,dp[j]*3,dp[k]*5}));
-            if(dp[i]*2 == dp.back()) i++;
-            if(dp[j]*3 == dp.back()) j++;
-            if(dp[k]*5 == dp.back()) k++;
+        while(v.size()<n){
+            int val = min({2*v[i],3*v[j],5*v[k]});
+            if(2*v[i]==val) i++;
+            if(3*v[j]==val) j++;
+            if(5*v[k]==val) k++;
+            v.push_back(val);
         }
-        return dp.back();   
+        // for(int val: v) cout<<val<<" ";
+        // cout<<"\n";
+        return v.back();
     }
 };
