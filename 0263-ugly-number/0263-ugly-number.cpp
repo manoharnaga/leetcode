98ca9a728a1c14a9eeb95133161adb243059c9f8
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-23 10:28:32 +0530

    Time: 2 ms (38.32%), Space: 7.5 MB (56.14%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        while(n>1 && n%2==0) n/=2;
-        while(n>1 && n%3==0) n/=3;
-        while(n>1 && n%5==0)  n/=5;
+        while(n && n%2==0) n/=2;
+        while(n && n%3==0) n/=3;
+        while(n && n%5==0) n/=5;
         
-        return (n==1);
+        if(n==1) return true;
+        return false;
     }
 };
