1a0a797d8f0e11462840a4bf4d55aab9617defa0
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-27 00:08:05 +0530

    Time: 94 ms (54.68%), Space: 34.2 MB (76.09%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,47 +1,32 @@
 class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& e) {
+        int n = maze.size();
+        int m = maze[0].size();
         queue<pair<int,int>> q;
         q.push({e[0],e[1]});
-		
-		//current moves
-        int moves=1;
-        int rows=maze.size();
-        int cols=maze[0].size();
-        
-		//to move in all directions
-        vector<vector<int>> offsets={{0,1},{1,0},{0,-1},{-1,0}};
-		
-        //mark the entrance  as visited
-        maze[e[0]][e[1]]='+';
-        while(!q.empty())
-        {
-            int l=q.size();
-			//for every node in the queue visit all of it's adjacent nodes which are not visited yet
-            for(int k=0;k<l;k++)
-            {
-                auto [i,j]=q.front();
+        vector<vector<int>> dir = {{1,0},{0,1},{-1,0},{0,-1}};
+
+        int moves = 1;
+        maze[e[0]][e[1]] = '+';
+        while(!q.empty()){
+            int sz = q.size();
+            // all nodes in cur level
+            for(int i=0;i<sz;i++){
+                auto [h1,h2] = q.front();
                 q.pop();
-                
-				//try all 4 directions from the current cell
-                for(int l=0;l<4;l++)
-                {
-                    int x=i+offsets[l][0];
-                    int y=j+offsets[l][1];
-					//a invalid move
-                    if(x<0 || y<0 || x>=rows || y>=cols || maze[x][y]=='+')
-                        continue;
-					//if we have reached the exit then current moves are the min moves to reach the exit
-                    if(x==0 || y==0 || x==rows-1 || y==cols-1)
-                        return moves;
-					//block the cell as we have visited
-                    maze[x][y]='+';
+                for(int l=0;l<4;l++){
+                    int x = h1+dir[l][0];
+                    int y = h2+dir[l][1];
+                    if(x<0 || x>=n || y<0 || y>=m || maze[x][y]=='+') continue; 
+                    if(x==0 || y==0 || x==(n-1) || y==(m-1)) return moves;
+
+                    maze[x][y] = '+';
                     q.push({x,y});
                 }
             }
-			//increment the moves
+            // at each level increase moves
             moves++;
-            
         }
         return -1;
     }
