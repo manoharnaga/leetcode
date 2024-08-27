d7c4825c34efab350739c14815a035209941dfad
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-27 14:04:19 +0530

    Time: 53 ms (93.82%), Space: 61.2 MB (14.90%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -3,20 +3,25 @@ public:
     int minCut(string s) {
         int n = s.size();
         vector<vector<int>> ispal(n,vector<int>(n,0));
-        
-        int d[n];
-        for(int i=n-1;i>=0;i--){
-            d[i] = n-i-1;
-            for(int j=i;j<n;j++){
-                if(s[i]==s[j] && (j-i<2 || ispal[i+1][j-1])){
+        for(int j=0;j<n;j++){
+            for(int i=0;i<=j;i++){
+                if(s[i]==s[j] && ((j-i)<2 || ispal[i+1][j-1])){
                     ispal[i][j] = 1;
-                    if(j==n-1){
-                        d[i] = 0;
-                    }
-                    else d[i] = min(d[i],1+d[j+1]);
                 }
             }
         }
-        return d[0];
+        vector<int> dp(n,INT_MAX);
+        for(int i=0;i<n;i++){
+            if(ispal[0][i]){
+                dp[i] = 1;
+                continue;
+            }
+            for(int j=0;j<i;j++){
+                if(ispal[j+1][i] && dp[j]!=INT_MAX){
+                    dp[i] = min(dp[i],1+dp[j]);
+                }
+            }
+        }
+        return dp[n-1]-1;
     }
 };
