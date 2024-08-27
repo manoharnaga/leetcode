601c7ace076268c01096d9c7ac22563908e66a25
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-27 22:20:00 +0530

    Time: 3 ms (88.22%), Space: 16 MB (43.49%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
         int valley = 0,peak = 0;
-        int i=0;
-        int ans = 0;
+        int n = prices.size();
+        int ans = 0,i = 0;
         while(i<n){
-            while(i+1<n && prices[i+1]<prices[i]){
-                i++;
-            }
+            while((i+1)<n && prices[i+1]<prices[i]) i++;
             valley = i;
-            while(i+1<n && prices[i+1]>prices[i]) i++;
+            while((i+1)<n && prices[i+1]>prices[i]) i++;
             peak = i;
             ans += (prices[peak]-prices[valley]);
             i++;
