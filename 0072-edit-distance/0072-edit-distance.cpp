0f650769743f1dfa4e633b6f8853a790353bf05d
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-08-27 14:36:14 +0530

    Time: 22 ms (22.59%), Space: 13.3 MB (24.22%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int rec(string& x,string& y,int n,int m,vector<vector<int>>& dp){
+    int rec(string& a,string& b,int n,int m,vector<vector<int>>& dp){
         if(n<0 || m<0) return max(n,m)+1;
-        
         if(dp[n][m]!=-1) return dp[n][m];
-        
-        int f1 = !(x[n]==y[m])+rec(x,y,n-1,m-1,dp);
-        int f2 = 1+rec(x,y,n-1,m,dp);
-        int f3 = 1+rec(x,y,n,m-1,dp);
+        int f1 = (a[n]!=b[m])+rec(a,b,n-1,m-1,dp);
+        int f2 = 1+rec(a,b,n-1,m,dp);
+        int f3 = 1+rec(a,b,n,m-1,dp);
         return dp[n][m] = min({f1,f2,f3});
     }
-    
     int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        int n,m;
+        n = word1.size();
+        m = word2.size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
         return rec(word1,word2,n-1,m-1,dp);
     }
 };
