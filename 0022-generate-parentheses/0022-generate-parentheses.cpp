f45f715943fc32543cd0769adc9e0c74b8b8e35a
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-09-06 12:35:54 +0530

    Time: 3 ms (60.80%), Space: 15.3 MB (61.11%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    int isValid(string& s){
-        int n = s.size();
-        stack<char> st;
-        for(int i=0;i<n;i++){
-            if(s[i]=='('){
-                st.push(s[i]);
-            }
-            else{
-                if(!st.empty()) st.pop();
-                else            return 0;
-            }
-        }
-        return st.empty();
-    }
+   
     vector<string> generateParenthesis(int n) {
-        vector<string> ans;
-        n = 2*n;
-        for(int b=0;b<(1<<n);b++){
-            string p;
-            for(int j=0;j<n;j++){
-                if(b & (1<<j)) p+="(";
-                else p+=")";
-            }
-            // cout<<p<<"\n";
-            if(isValid(p)) ans.push_back(p);
-        }
-        return ans;
+        vector<string> ret;
+        helper(ret, "", n, n);
+        return ret;
     }
+    void helper(vector<string> & res, string str, int left, int right){
+        if(left == 0 && right == 0){
+            res.push_back(str);
+            return;
+        }
+        if(left > 0) helper(res, str + "(", left - 1, right);
+        if(right > left) helper(res, str + ")", left, right - 1);
+    }
+
 };
