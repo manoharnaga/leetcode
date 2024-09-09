f9cdd1e3e94eb33bec36910760bca24b2347517b
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-09-09 11:30:41 +0530

    Time: 146 ms (18.69%), Space: 52.7 MB (11.96%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     int nearestExit(vector<vector<char>>& a, vector<int>& e) {
+        // level order traversal
         int n = a.size();
         int m = a[0].size();
         vector<vector<int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
-        queue<pair<pair<int,int>,int>> q;
-        q.push({{e[0],e[1]},0});
+        queue<pair<int,int>> q;
+        q.push({e[0],e[1]});
         a[e[0]][e[1]] = '+';
+        int moves = -1;
         while(!q.empty()){
-            auto [x,y] = q.front().first;
-            int moves = q.front().second;
-            q.pop();
-            for(auto it: dir){
-                int i = x+it[0];
-                int j = y+it[1];
-                if(i<0 || j<0 || i==n || j==m){
-                    if(x!=e[0] || y!=e[1])  return moves;
-                }
-                else if(a[i][j]=='.'){  
-                    a[i][j] = '+';
-                    q.push({{i,j},moves+1});
+            int sz = q.size();
+            moves++;
+            for(int k=0;k<sz;k++){
+                auto [x,y] = q.front();
+                q.pop();
+                for(auto it: dir){
+                    int i = x+it[0];
+                    int j = y+it[1];
+                    if(i<0 || j<0 || i==n || j==m){
+                        if(x!=e[0] || y!=e[1]) return moves;
+                    }
+                    else if(a[i][j]=='.'){
+                        a[i][j] = '+';
+                        q.push({i,j});
+                    }
                 }
             }
         }
