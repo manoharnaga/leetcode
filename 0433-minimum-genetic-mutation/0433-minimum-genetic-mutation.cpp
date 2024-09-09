1477c3a428af89fda6dd1529e23c720880eca8d7
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-09-09 11:49:07 +0530

    Time: 0 ms (100.00%), Space: 8 MB (95.50%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int ham(string& x,string& y){
+        int cnt = 0;
+        for(int i=0;i<x.size();i++){
+            if(x[i]!=y[i]) cnt++;
+        }
+        return cnt;
+    }
+
+    int minMutation(string src, string dest, vector<string>& bank) {
+        int m = bank.size();
+        int idx = -1;
+        for(int i=0;i<m;i++){
+            if(bank[i]==dest){
+                idx = i;
+                break;
+            }
+        }
+        if(idx==-1) return -1;
+
+        vector<int> adj[m+1];
+
+        for(int i=0;i<m;i++){
+            for(int j=i+1;j<m;j++){
+                if(ham(bank[i],bank[j])==1){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            if(ham(src,bank[i])==1){
+                adj[m].push_back(i);
+                adj[i].push_back(m);
+            }
+        }
+
+        vector<int> vist(m+1,0);
+        queue<int> q;
+        q.push(m);
+        vist[m] = 1;
+        int moves = 0;
+        while(!q.empty()){
+            int sz = q.size();
+            for(int i=0;i<sz;i++){
+                int at = q.front();
+                q.pop();
+                if(at==idx) return moves;
+                for(auto it: adj[at]){
+                    if(!vist[it]){
+                        vist[it] = 1;
+                        q.push(it);
+                    }
+                }
+            }
+            moves++;
+        }
+        return -1;
+    }
+};
