bc6d5fc7aa5aa436ee42f7b24365c61b34e24a02
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-09-10 12:17:14 +0530

    Time: 32 ms (50.83%), Space: 17.1 MB (89.55%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
-    int maximalRectangle(vector<vector<char>>& mat){
-        int n = mat.size();
-        int m = mat[0].size();
-        vector<int> right(m),left(m),height(m);
+    int maximalRectangle(vector<vector<char>>& a) {
+        int n = a.size();
+        int m = a[0].size();
+        vector<int> height(m,0),left(m),right(m);
         int ans = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(mat[i][j]=='1') height[j]++;
-                else               height[j] = 0;
+                if(a[i][j]=='1') height[j]++;
+                else        height[j]=0;
             }
-            // largest rectangle in a histogram
-            // nse
             stack<int> st;
             for(int j=0;j<m;j++){
                 while(!st.empty() && height[st.top()]>=height[j]) st.pop();
-                if(st.empty()) left[j]=-1;
+                if(st.empty()) left[j] = -1;
                 else           left[j] = st.top();
                 st.push(j);
             }
+
             while(!st.empty()) st.pop();
             for(int j=m-1;j>=0;j--){
                 while(!st.empty() && height[st.top()]>=height[j]) st.pop();
-                if(st.empty()) right[j]=m;
-                else           right[j]=st.top();
+                if(st.empty()) right[j] = m;
+                else           right[j] = st.top();
                 st.push(j);
             }
-            // for(int j=0;j<m;j++) cout<<height[j]<<" ";
-            // cout<<"\n";
-            
+
             for(int j=0;j<m;j++){
-                int val = height[j]*(right[j]-1-(left[j]+1)+1);
-                // cout<<val<<" ";
+                int val = height[j] * (right[j]-1-(left[j]+1)+1);
                 ans = max(ans,val);
             }
-            // cout<<"\n";
         }
         return ans;
     }
