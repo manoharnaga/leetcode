215f2e29ab3dbbfa059d499d31706e8f4337174f
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-09-10 12:01:51 +0530

    Time: 19 ms (62.03%), Space: 20.7 MB (59.77%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    int rec(string& s1,string& s2,int len,int i,int j,vector<vector<vector<int>>>& dp){
-        if(len<1) return 1;
-        if(dp[len][i][j]!=-1) return dp[len][i][j];
-        if(s1.substr(i,len)==s2.substr(j,len)) return dp[len][i][j] = 1;
-
+    int rec(string& s1,string& s2,int i,int j,int len,vector<vector<vector<int>>>& dp){
+        if(len==1) return s1[i]==s2[j];
+        
+        if(dp[i][j][len]!=-1) return dp[i][j][len];
+        
+        if(s1.substr(i,len)==s2.substr(j,len)) return 1;
+        
         for(int k=1;k<len;k++){
-            int f1 = rec(s1,s2,k,i,j,dp) && rec(s1,s2,len-k,i+k,j+k,dp);
-            int f2 = rec(s1,s2,k,i,j+len-k,dp) && rec(s1,s2,len-k,i+k,j,dp);
-            if(f1 || f2) return dp[len][i][j] = true;
+            int f1 = rec(s1,s2,i,j,k,dp) && rec(s1,s2,i+k,j+k,(len-k),dp);
+            int f2 = rec(s1,s2,i,j+len-k,k,dp) && rec(s1,s2,i+k,j,(len-k),dp);
+            if(f1 || f2) return dp[i][j][len] = true;
         }
-        return dp[len][i][j] = false;
+        return dp[i][j][len] = false;
     }
+    
     bool isScramble(string s1, string s2) {
         int n = s1.size();
-        vector dp(n+1,vector<vector<int>>(n,vector<int>(n,-1)));
-        return rec(s1,s2,n,0,0,dp);
+        vector dp(n,vector<vector<int>>(n,vector<int>(n+1,-1)));
+        return rec(s1,s2,0,0,n,dp);
     }
 };
