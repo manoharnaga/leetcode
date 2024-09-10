7f1fc1e08df71d0a3e1d3add74ebc1123e317fd3
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-09-10 11:00:06 +0530

    Time: 366 ms (5.03%), Space: 133.5 MB (5.27%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,26 +1,42 @@
 class Solution {
 public:
     typedef long long ll;
-    long long totalCost(vector<int>& costs, int k, int candidates) {
-        int n = costs.size();
-        priority_queue<ll,vector<ll>,greater<ll>> pq1,pq2;
-        int i=0,j = n-1,cnt = 0;
+    long long totalCost(vector<int>& c, int k, int candidates) {
+        int n = c.size();
+        vector<int> vist(n,0);
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+        int i=0,j=n-1;
+        for(i=0;i<candidates;i++){
+            vist[i] = 1;
+            pq.push({c[i],i,+1});
+        }
+        i--;
+        
+        int f = 0;
+        for(j=n-1;j>=(n-candidates);j--){
+            if(vist[j]){
+                j++;
+                f = 1;
+                break;
+            }
+            vist[j] = 1;
+            pq.push({c[j],j,-1});
+        }
+        if(!f) j++;
+        
         ll ans = 0;
-        while(cnt<k){
-            while(pq1.size()<candidates && i<=j) pq1.push(costs[i++]);
-            while(pq2.size()<candidates && j>=i) pq2.push(costs[j--]);
-            
-            ll a = (pq1.empty() ? INT_MAX: pq1.top());
-            ll b = (pq2.empty() ? INT_MAX: pq2.top());
-            if(a<=b){
-                ans += a;
-                pq1.pop();
+        for(int p=0;p<k;p++){
+            auto it = pq.top();
+            pq.pop();
+            ans += (ll)it[0];
+            if(it[2]==1){
+                i++;
+                if(i<j) pq.push({c[i],i,1});
             }
             else{
-                ans += b;
-                pq2.pop();
+                j--;
+                if(i<j) pq.push({c[j],j,-1});
             }
-            cnt++;
         }
         return ans;
     }
