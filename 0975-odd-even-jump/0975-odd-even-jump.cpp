97d9dfde7abc828b92e6359e7061cb906a393af8
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-10-13 11:58:54 +0530

    Time: 63 ms (73.94%), Space: 31.4 MB (52.00%) - LeetHub

--- 0975-odd-even-jump/0975-odd-even-jump.cpp
+++ 0975-odd-even-jump/0975-odd-even-jump.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int oddEvenJumps(vector<int>& A) {
-        int n  = A.size(), res = 1;
-        vector<int> higher(n), lower(n);
-        higher[n - 1] = lower[n - 1] = 1;
-        map<int, int> map;
-        map[A[n - 1]] = n - 1;
-        for (int i = n - 2; i >= 0; --i) {
-            auto hi = map.lower_bound(A[i]), lo = map.upper_bound(A[i]);
-            if (hi != map.end()) higher[i] = lower[hi->second];
-            if (lo != map.begin()) lower[i] = higher[(--lo)->second];
-            if (higher[i]) res++;
-            map[A[i]] = i;
+    int oddEvenJumps(vector<int>& a) {
+        int n = a.size();
+        vector<int> higher(n),lower(n);
+        higher[n-1] = lower[n-1] = 1;
+        map<int,int> mp;
+        mp[a[n-1]] = n-1;
+        int res = 1;
+        for(int i=n-2;i>=0;i--){
+            auto hi = mp.lower_bound(a[i]), lo = mp.upper_bound(a[i]);
+            if(hi != mp.end()) higher[i] = lower[hi->second];
+            if(lo != mp.begin()) lower[i] = higher[(--lo)->second];
+            if(higher[i]) res++;
+            
+            mp[a[i]] = i;
         }
         return res;
     }
