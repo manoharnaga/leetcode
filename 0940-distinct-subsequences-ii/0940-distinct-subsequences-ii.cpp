d74cb90ad97e68fc12e047bdb2a28d307861ed8c
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-10-20 15:19:19 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (73.05%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -4,20 +4,28 @@ public:
         const int N = s.length();
         const int MOD = 1e9 + 7;
         
-        vector<int> dp(N+1);
-        dp[0] = 1;
+        vector<int> dp(N);
+        dp[0] = 2;
         vector<int> last(26, -1);
+        last[s[0]-'a'] = 0;
         
-        for(int i = 0; i < N; i++){
+        // cout<<0<<" "<<-1<<" "<<dp[0]<<"\n";
+        for(int i = 1; i < N; i++){
             int x = s[i] - 'a';
-            dp[i+1] = dp[i] * 2 % MOD;
+            dp[i] = dp[i-1] * 2 % MOD;
             if(last[x] >= 0) // if this is the first occurence of ch
-                dp[i+1] -= dp[last[x]];
-            dp[i+1] %= MOD;
+                if(last[x]>0)
+                    dp[i] -= dp[last[x]-1];
+                else
+                    dp[i]--; // empty string
+            
+            // cout<<i<<" "<<last[x]-1<<" "<<dp[i]<<"\n";
+            
+            dp[i] %= MOD;
             last[x] = i;
         }
-        dp[N]--;
-        if(dp[N] < 0) dp[N] += MOD;
-        return dp[N];
+        dp[N-1]--;
+        if(dp[N-1] < 0) dp[N-1] += MOD;
+        return dp[N-1];
     }
 };
