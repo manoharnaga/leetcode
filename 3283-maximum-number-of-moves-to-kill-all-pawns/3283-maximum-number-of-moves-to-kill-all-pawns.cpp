a7068ea2b4e614d598912931f3d0b832100b9dc6
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-10-23 14:20:40 +0530

    Time: 716 ms (71.47%), Space: 344.5 MB (30.96%) - LeetHub

--- 3283-maximum-number-of-moves-to-kill-all-pawns/3283-maximum-number-of-moves-to-kill-all-pawns.cpp
+++ 3283-maximum-number-of-moves-to-kill-all-pawns/3283-maximum-number-of-moves-to-kill-all-pawns.cpp
@@ -1,42 +1,85 @@
 class Solution {
 public:
-    int moves[17][17], dp[17][32767] = {};
-int knight[9] = {1, 2, 1, -2, -1, 2, -1, -2, 1}; 
-void bfs(int i, vector<vector<int>> &pos) {
-    int b[50][50] = {}, sx = pos[i][0], sy = pos[i][1];
-    vector<pair<int, int>> q{{sx, sy}};
-    while (!q.empty()) {
-        vector<pair<int, int>> q1;
-        for (auto [x, y] : q)
-            for (int d = 0; d + 1 < 9; ++d) {
-                int dx = x + knight[d], dy = y + knight[d + 1];
-                if (min(dx, dy) >= 0 && max(dx, dy) < 50 && !(dx == sx && dy == sy) && b[dx][dy] == 0) {
-                    b[dx][dy] = b[x][y] + 1;
-                    q1.push_back({dx, dy});
+    int dp[17][32767];
+    int moves[17][17];
+    int n;
+    
+    int knight[9] = {1, 2, 1, -2, -1, 2, -1, -2, 1}; 
+
+    void bfs(int i, vector<vector<int>> &pos) {
+        int b[50][50] = {}, sx = pos[i][0], sy = pos[i][1];
+        vector<pair<int, int>> q{{sx, sy}};
+        while (!q.empty()) {
+            vector<pair<int, int>> q1;
+            for (auto [x, y] : q)
+                for (int d = 0; d + 1 < 9; ++d) {
+                    int dx = x + knight[d], dy = y + knight[d + 1];
+                    if (min(dx, dy) >= 0 && max(dx, dy) < 50 && !(dx == sx && dy == sy) && b[dx][dy] == 0) {
+                        b[dx][dy] = b[x][y] + 1;
+                        q1.push_back({dx, dy});
+                    }
+                }
+            swap(q, q1);
+        }
+        for (int j = i + 1; j < pos.size(); ++j)
+            moves[i][j] = moves[j][i] = b[pos[j][0]][pos[j][1]];        
+    }
+
+    int func(int kx,int ky,int x,int y){
+        if(kx<0 || ky<0 || kx==50 || ky==50) return INT_MAX;
+        
+        int vist[50][50];memset(vist,0,sizeof(vist));
+        
+        vector<vector<int>> dir = {{2,1},{2,-1},{-2,1},{-2,-1},{1,2},{1,-2},{-1,2},{-1,-2}};
+        queue<pair<int,int>> q;
+        q.push({kx,ky});
+        int moves = 0;
+        while(!q.empty()){
+            int sz = q.size();
+            moves++;
+            for(int i=0;i<sz;i++){
+                auto [hx,hy] = q.front();
+                q.pop();
+                for(auto it: dir){
+                    int nx = hx+it[0];
+                    int ny = hy+it[1];
+                    if(nx<0 || ny<0 || nx>=50 || ny>=50 || vist[nx][ny]) continue;
+                    if(nx==x && ny==y) return moves;
+                    vist[nx][ny] = 1;
+                    q.push({nx,ny});
                 }
             }
-        swap(q, q1);
-    }
-    for (int j = i + 1; j < pos.size(); ++j)
-        moves[i][j] = moves[j][i] = b[pos[j][0]][pos[j][1]];        
-}
-int dfs(int i, unsigned int mask, int turns, int sz) {
-    if (turns == sz)
+        }
         return 0;
-    if (dp[i][mask] == 0) {
-        dp[i][mask] = turns % 2 ? INT_MAX : 0;
-        for (int j = 0; j < sz; ++j)
-            if (((1 << j) & mask) == 0) {
-                int resj = moves[i][j] + dfs(j, mask + ((1 << j)), turns + 1, sz);
-                dp[i][mask] = turns % 2 ?  min(dp[i][mask], resj) : max(dp[i][mask], resj);
-            }
     }
-    return dp[i][mask];
-}
-int maxMoves(int kx, int ky, vector<vector<int>>& positions) {
-    positions.push_back({kx, ky});
-    for (int i = 0; i < positions.size(); ++i)
-        bfs(i, positions);
-    return dfs(positions.size() - 1, 0, 0, positions.size() - 1);
-}
+    
+    int rec(int idx,int turn,int pawns,int mask){
+        if(pawns==0) return 0;
+        
+        if(dp[idx][mask]!=-1) return dp[idx][mask];
+        
+        int maxval = 0,minval = INT_MAX;
+        for(int i=0;i<n-1;i++){
+            if(((1<<i) & mask) == 0){
+                int ret = moves[idx][i]+rec(i,!turn,pawns-1,mask+(1<<i));
+                if(turn==0) maxval = max(maxval,ret);
+                else        minval = min(minval,ret);
+            }
+        }
+        
+        return dp[idx][mask] = (turn ? minval: maxval);
+    }
+    
+    int maxMoves(int kx, int ky, vector<vector<int>>& pos) {
+        pos.push_back({kx,ky});
+        n = pos.size();
+        memset(dp,-1,sizeof(dp));
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         moves[i][j] = moves[j][i] = func(pos[i][0],pos[i][1],pos[j][0],pos[j][1]);
+        //     }
+        // }
+        for(int i=0;i<n;i++) bfs(i,pos);
+        return rec(n-1,0,n-1,0);
+    }
 };
