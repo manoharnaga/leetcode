02f0b2ad8f66fe8861b33e58f4a3f049ab207794
Manohar Naga <83327200+manoharnaga@users.noreply.github.com>
2024-11-17 19:33:32 +0530

    Time: 0 ms (100.00%), Space: 14 MB (52.73%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -3,22 +3,24 @@ public:
     int search(vector<int>& a, int x) {
         int low = 0,high = a.size()-1;
         while(low<=high){
-            int mid = (low+high)/2;
-            // cout<<low<<" "<<high<<" "<<mid<<"\n";
-            if(a[mid]==x){
-                return mid;
-            }
-            else if(a[mid]<x && x<=a[high]){
-                low = mid+1;
-            }
-            else if(a[low]<=x && x<a[mid]){
-                high = mid-1;
-            }
-            else if(a[mid]<=a[high]){
-                high = mid-1;
+            int mid = (high+low)/2;
+            if(a[mid]==x) return mid;
+            
+            if(a[low]<=a[mid]){
+                if(a[low]<=x && x<=a[mid]){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
             }
             else{
-                low = mid+1;
+                if(a[mid]<=x && x<=a[high]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
             }
         }
         return -1;
